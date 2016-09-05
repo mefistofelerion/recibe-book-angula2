6e5c1a484c1f802ef0545f1f644722887d1cfc7e
Ivan Guerra <ivan.aguerra616@gmail.com>
2016-09-05 16:17:44 -0500

    Added header, recipe list and recipe details.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes'
+import {ShoppingListComponent} from "./shopping-list";
 
 @Component({
   moduleId: module.id,
   selector: 'recipe-book-app',
-  templateUrl: 'recipe-book.component.html'
+  templateUrl: 'recipe-book.component.html',
+  directives: [HeaderComponent, RecipesComponent, ShoppingListComponent]
 })
 export class AppComponent {
 
